22767927942375214c70c4c4e8f2f9369f36147f
tthornton8 <timothythornton8@gmail.com>
2022-03-31 01:38:56 +0100

    JQuery to refresh stepper angles

--- src/main.cpp
+++ src/main.cpp
@@ -291,6 +291,16 @@ void setup()
     request->send(LittleFS, "/style.css", "text/css");
   });
 
+  // Forward Sensor Data
+  server.on("/motor_pos", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "application/json", "{\"RA\": " + String(stepperEq.degrees, 2) + ", \"DEC\": " + String(stepperDec.degrees, 2) + "}"); 
+  });
+
+  server.on("/js/jquery.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/js/jquery.js", "text/javascript");
+});
+
+  // Handle mtor Move
   server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
     String inputValue;
     String inputMotor;
